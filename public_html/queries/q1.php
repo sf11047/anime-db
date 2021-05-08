221af165d6551e19834082a7b7f690f62e4fef4c
Samantha Fu <zongming04@gmail.com>
2021-05-08 19:11:15 -0400

    trying to let all genre

--- public_html/queries/q1.php
+++ public_html/queries/q1.php
@@ -13,6 +13,12 @@ include '../php/open.php';
     echo "genre: ".$genreSelection." <br> </br>";
     
     $myQuery = "WITH PopularByGenre AS (SELECT MIN(AllMedia.rank) AS rank, BelongsTo.genreName FROM AllMedia JOIN BelongsTo on AllMedia.mediaID = BelongsTo.mediaID GROUP BY BelongsTo.genreName) SELECT PopularByGenre.genreName, AllMedia.mediaID, AllMedia.titleJPN, AllMedia.synopsis, AllMedia.rank, AllMedia.startDate, AllMedia.source FROM BelongsTo JOIN AllMedia ON BelongsTo.mediaID = AllMedia.mediaID JOIN PopularByGenre ON PopularByGenre.genreName = BelongsTo.genreName AND PopularByGenre.rank = AllMedia.rank WHERE PopularByGenre.genreName = ?;";
+    
+    
+    if ($genreSelection == "All") {
+        echo substr($myQuery, 0, 100);
+    }
+    
     $stmt= $conn->prepare($myQuery);
     $stmt->bind_param("s", $genreSelection);
     $stmt->execute();
