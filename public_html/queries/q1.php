0c7015154b72d91509140f5f563d7f80f0d6dd84
Samantha Fu <zongming04@gmail.com>
2021-05-08 19:43:23 -0400

    kill

--- public_html/queries/q1.php
+++ public_html/queries/q1.php
@@ -14,7 +14,7 @@ include '../php/open.php';
 
     $myQuery = "WITH PopularByGenre AS (SELECT MIN(AllMedia.rank) AS rank, BelongsTo.genreName FROM AllMedia JOIN BelongsTo on AllMedia.mediaID = BelongsTo.mediaID GROUP BY BelongsTo.genreName) SELECT PopularByGenre.genreName, AllMedia.mediaID, AllMedia.titleJPN, AllMedia.synopsis, AllMedia.rank, AllMedia.startDate, AllMedia.source FROM BelongsTo JOIN AllMedia ON BelongsTo.mediaID = AllMedia.mediaID JOIN PopularByGenre ON PopularByGenre.genreName = BelongsTo.genreName AND PopularByGenre.rank = AllMedia.rank;";
     $stmt = $conn->prepare($myQuery); 
-
+    $stmt->execute();
 
     echo "Finished";
 
