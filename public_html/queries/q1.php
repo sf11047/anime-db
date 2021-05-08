b1ac847d149b9ff49bdf212f7ab79c4ef59d05bf
Samantha Fu <zongming04@gmail.com>
2021-05-08 18:04:05 -0400

    test

--- public_html/queries/q1.php
+++ public_html/queries/q1.php
@@ -9,6 +9,13 @@ include '../php/open.php';
     $query = "Test Question";
     echo "<h1>".$query."<h1>";
 
+
+    $stmt = $conn->prepare("SELECT * FROM TV LIMIT 5");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    echo $result;
+
+    echo "finished";
 //End Query Code
 
 $conn->close();
