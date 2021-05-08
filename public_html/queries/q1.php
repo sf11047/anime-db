5e186cd8b210d7d0060c1004f91799e7076dd937
Samantha Fu <zongming04@gmail.com>
2021-05-08 18:52:37 -0400

    get select value

--- public_html/queries/q1.php
+++ public_html/queries/q1.php
@@ -8,6 +8,9 @@ include '../php/open.php';
 
     $query = "Test Question";
     echo "<h1>".$query."<h1>";
+
+    $genre = $_POST['genre'];
+    echo $genre;
     
     $myQuery = "WITH PopularByGenre AS (SELECT MIN(AllMedia.rank) AS rank, BelongsTo.genreName FROM AllMedia JOIN BelongsTo on AllMedia.mediaID = BelongsTo.mediaID GROUP BY BelongsTo.genreName) SELECT PopularByGenre.genreName, AllMedia.mediaID, AllMedia.titleJPN, AllMedia.synopsis, AllMedia.rank, AllMedia.startDate, AllMedia.source FROM BelongsTo JOIN AllMedia ON BelongsTo.mediaID = AllMedia.mediaID JOIN PopularByGenre ON PopularByGenre.genreName = BelongsTo.genreName AND PopularByGenre.rank = AllMedia.rank;";
     $stmt= $conn->prepare($myQuery);
