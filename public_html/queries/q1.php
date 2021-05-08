297863f857b2012052daad435b509928c5df796f
Samantha Fu <zongming04@gmail.com>
2021-05-08 18:45:12 -0400

    add actual query

--- public_html/queries/q1.php
+++ public_html/queries/q1.php
@@ -9,12 +9,12 @@ include '../php/open.php';
     $query = "Test Question";
     echo "<h1>".$query."<h1>";
     
-    $myQuery = "SELECT * FROM TV Limit 5;";
+    $myQuery = "WITH PopularByGenre AS (SELECT MIN(AllMedia.rank) AS rank, BelongsTo.genreName FROM AllMedia JOIN BelongsTo on AllMedia.mediaID = BelongsTo.mediaID GROUP BY BelongsTo.genreName) SELECT PopularByGenre.genreName, AllMedia.mediaID, AllMedia.titleJPN, AllMedia.synopsis, AllMedia.rank, AllMedia.startDate, AllMedia.source FROM BelongsTo JOIN AllMedia ON BelongsTo.mediaID = AllMedia.mediaID JOIN PopularByGenre ON PopularByGenre.genreName = BelongsTo.genreName AND PopularByGenre.rank = AllMedia.rank;";
     $stmt= $conn->prepare($myQuery);
     $stmt->execute();
     $result = $stmt->get_result(); 
     while ($row = $result->fetch_assoc()) {
-        echo $row['titleJPN'];
+        echo $row['genreName'];
     }
 
     echo "done";
