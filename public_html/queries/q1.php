99e3b4447c711875e53a5e60409a23c8582f82ad
Samantha Fu <zongming04@gmail.com>
2021-05-08 19:25:04 -0400

    revert

--- public_html/queries/q1.php
+++ public_html/queries/q1.php
@@ -13,11 +13,9 @@ include '../php/open.php';
     echo "genre: ".$genreSelection." <br> </br>";
     
 
-    $myQuery = "WITH PopularByGenre AS (SELECT MIN(AllMedia.rank) AS rank, BelongsTo.genreName FROM AllMedia JOIN BelongsTo on AllMedia.mediaID = BelongsTo.mediaID GROUP BY BelongsTo.genreName) SELECT PopularByGenre.genreName, AllMedia.mediaID, AllMedia.titleJPN, AllMedia.synopsis, AllMedia.rank, AllMedia.startDate, AllMedia.source FROM BelongsTo JOIN AllMedia ON BelongsTo.mediaID = AllMedia.mediaID JOIN PopularByGenre ON PopularByGenre.genreName = BelongsTo.genreName AND PopularByGenre.rank = AllMedia.rank WHERE PopularByGenre.genreName = ?;";
+    $myQuery = "WITH PopularByGenre AS (SELECT MIN(AllMedia.rank) AS rank, BelongsTo.genreName FROM AllMedia JOIN BelongsTo on AllMedia.mediaID = BelongsTo.mediaID GROUP BY BelongsTo.genreName) SELECT PopularByGenre.genreName, AllMedia.mediaID, AllMedia.titleJPN, AllMedia.synopsis, AllMedia.rank, AllMedia.startDate, AllMedia.source FROM BelongsTo JOIN AllMedia ON BelongsTo.mediaID = AllMedia.mediaID JOIN PopularByGenre ON PopularByGenre.genreName = BelongsTo.genreName AND PopularByGenre.rank = AllMedia.rank;";
     $stmt = $conn->prepare($myQuery);
 
-    $stmt->bind_param("s", $genreSelection);
-    
     $stmt->execute();
     $result = $stmt->get_result(); 
     while ($row = $result->fetch_assoc()) {
