fdea2a0adfb0399e82562324a06a60a3f813806a
Samantha Fu <zongming04@gmail.com>
2021-05-09 01:54:32 -0400

    get rid of graph

--- public_html/queries/q7.php
+++ public_html/queries/q7.php
@@ -31,29 +31,6 @@ include '../php/open.php';
     }
 
 //End Query Code
-
-?>
-<script>
-        window.onload = function () { 
-            var chart = new CanvasJS.Chart("chartContainer", {
-                animationEnabled: false,
-                exportEnabled: true,
-                theme: "light1", // "light1", "light2", "dark1", "dark2"
-                title:{
-                    text: "Most Popular Anime Sources"
-                },
-                data: [{
-                    type: "column", //change type to column, bar, line, area, pie, etc  
-                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-                }]
-            });
-            chart.render(); 
-        }
-    </script>
-    <div id="chartContainer" style="height: 400px; width: 100%;"></div>
-	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-
-<?php
-$conn->close();
-include "../php/query-template-end.php"; //HTML Template
+    $conn->close();
+    include "../php/query-template-end.php"; //HTML Template
 ?>
