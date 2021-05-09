53dbd3c3c1d62189ad87ffe7821c0a5033973350
Samantha Fu <zongming04@gmail.com>
2021-05-08 20:00:23 -0400

    test

--- public_html/queries/q1.php
+++ public_html/queries/q1.php
@@ -12,7 +12,7 @@ include '../php/open.php';
     $genreSelection = $_POST['genre'];
     echo "genre: ".$genreSelection." <br> </br>";
 
-    $myQuery = "SELECT * FROM TV LIMIT 10;";
+    $myQuery = "WITH PopularByGenre AS (SELECT MIN(AllMedia.rank) AS rank, BelongsTo.genreName FROM AllMedia JOIN BelongsTo on AllMedia.mediaID = BelongsTo.mediaID GROUP BY BelongsTo.genreName) SELECT PopularByGenre.genreName, AllMedia.mediaID, AllMedia.titleJPN, AllMedia.synopsis, AllMedia.rank, AllMedia.startDate, AllMedia.source FROM BelongsTo JOIN AllMedia ON BelongsTo.mediaID = AllMedia.mediaID JOIN PopularByGenre ON PopularByGenre.genreName = BelongsTo.genreName AND PopularByGenre.rank = AllMedia.rank;";
     $stmt = $conn->prepare($myQuery); 
     $stmt->execute();
     $result = $stmt->get_result();
