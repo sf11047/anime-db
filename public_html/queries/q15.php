fc38d910d18ec5068aa92e00edf8d7245fdc7fba
Anthony D <adas22@jhu.edu>
2021-05-09 00:35:05 -0400

    here goes nothing

--- public_html/queries/q15.php
+++ public_html/queries/q15.php
@@ -7,6 +7,19 @@ include '../php/open.php';
 
     echo "<h1>What shows have the best animation? Best story? Best characters?<h1>"; //First line should be the question
 
+    $rating = $_POST['q15-rating'];
+
+    $myQuery = "Call TopCategoryRating(?);";
+    $stmt = $conn->prepare($myQuery); 
+    $stmt->bind_param("s", $rating);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        echo "<h2>".$row['titleJPN']."</h2>";
+        echo "<h2>".$row['avgRating']."</h2>";
+        echo "<h2>".$row['mediaID']."</h2>";
+    }
+
 //End Query PHP Code
 
 ?>
