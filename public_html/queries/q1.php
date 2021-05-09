dd595f3cf0014063ed13f78da385934c68d330f8
Samantha Fu <zongming04@gmail.com>
2021-05-08 20:48:00 -0400

    typo lmfao

--- public_html/queries/q1.php
+++ public_html/queries/q1.php
@@ -12,12 +12,13 @@ include '../php/open.php';
     $genreSelection = $_POST['genre'];
     echo "genre: ".$genreSelection." <br> </br>";
 
-    $myQuery = "Call PopByGenre('All');";
+    $myQuery = "Call PopByGenre(?);";
     $stmt = $conn->prepare($myQuery); 
+    $stmt->bind_param("s", $genreSelection);
     $stmt->execute();
     $result = $stmt->get_result();
     while ($row = $result->fetch_assoc()) {
-        echo "genre: ".$row['genreName']." "$row['titleJPN']."<br></br>";
+        echo "genre: ".$row['genreName']." ".$row['titleJPN']."<br></br>";
     }
     
 //End Query Code
