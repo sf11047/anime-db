a8693e8e1c2759c20bc48823c2a56fc7e8350e8e
Samantha Fu <zongming04@gmail.com>
2021-05-09 02:06:34 -0400

    q8 procedures

---
+++ public_html/queries/q8.php
@@ -0,0 +1,48 @@
+<?php
+
+include "../php/query-template.php"; //HTML Template
+
+include '../php/open.php';
+
+//Begin Query Code
+
+    $query = "Number of Shows with Higest Rating Per Review Category";
+    echo "<h1>".$query."<h1>";
+
+    $dataPoints = array();
+
+    $myQuery = "CALL HighestReviewCategory();";
+    $stmt = $conn->prepare($myQuery); 
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        array_push($dataPoints, array( "label"=> $row["category"], "y"=> $row["numShows"]));
+    }
+    
+//End Query Code
+
+?>
+<script>
+        window.onload = function () { 
+            var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: false,
+                exportEnabled: true,
+                theme: "light1", // "light1", "light2", "dark1", "dark2"
+                title:{
+                    text: "Number of Shows Most Highly Reviewed In Each Review Category"
+                },
+                data: [{
+                    type: "column", //change type to column, bar, line, area, pie, etc  
+                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                }]
+            });
+            chart.render(); 
+        }
+    </script>
+    <div id="chartContainer" style="height: 400px; width: 100%;"></div>
+	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+<?php
+$conn->close();
+include "../php/query-template-end.php"; //HTML Template
+?>
