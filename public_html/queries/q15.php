3c4c0faf77f30395f3266371c12e239bb611728d
Anthony D <adas22@jhu.edu>
2021-05-09 01:49:57 -0400

    okay things are working

--- public_html/queries/q15.php
+++ public_html/queries/q15.php
@@ -10,26 +10,16 @@ include '../php/open.php';
     echo "<h1>".$query."<h1>";
     
 
-    $genreSelection = $_POST['q15'];
-    $genreSelection  = "All";
-    if ($genreSelection != "All") {
-        echo "<h1> Genre: ".$genreSelection."<h1>";
-    }
+    $rating = $_POST['q15'];
+    $rating  = "All";
 
     $myQuery = "Call PopByGenre(?);";
     $stmt = $conn->prepare($myQuery); 
-    $stmt->bind_param("s", $genreSelection);
+    $stmt->bind_param("s", $rating);
     $stmt->execute();
     $result = $stmt->get_result();
     while ($row = $result->fetch_assoc()) {
-        if ($genreSelection == "All") {
-            echo "<h1> Genre: ".$row['genreName']."<h1>";
-        }
         echo "<h2>".$row['titleJPN']."</h2>";
-        echo "<h3> Rank: ".$row['rank']."</h3>";
-        echo "<h3> Start Date: ".$row['startDate']."</h3>";
-        echo "<h3> Source: ".$row['source']."</h3>";
-        echo "<p>".$row['synopsis']."</p>";
     }
     
 //End Query Code
