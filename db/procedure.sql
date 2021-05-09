8fb076f74e28cddb9bdfb8505bfcb04137373b07
Samantha Fu <zongming04@gmail.com>
2021-05-09 00:00:47 -0400

    stored procedures

---
+++ db/procedure.sql
@@ -0,0 +1,38 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS PopByGenre //
+
+CREATE PROCEDURE PopByGenre(IN genre VARCHAR(30))
+BEGIN
+    IF genre = "ALL" THEN
+WITH PopularByGenre AS (
+        SELECT MIN(AllMedia.rank) AS rank, BelongsTo.genreName 
+        FROM AllMedia JOIN BelongsTo on AllMedia.mediaID = BelongsTo.mediaID
+        GROUP BY BelongsTo.genreName)
+SELECT PopularByGenre.genreName, AllMedia.mediaID, AllMedia.titleJPN, AllMedia.synopsis, AllMedia.rank, AllMedia.startDate, AllMedia.source
+FROM BelongsTo JOIN AllMedia ON BelongsTo.mediaID = AllMedia.mediaID JOIN PopularByGenre ON PopularByGenre.genreName = BelongsTo.genreName AND PopularByGenre.rank = AllMedia.rank;
+    ELSEIF EXISTS(SELECT genre FROM Genre WHERE genre = Genre.genreName) THEN
+WITH PopularByGenre AS (
+        SELECT MIN(AllMedia.rank) AS rank, BelongsTo.genreName 
+        FROM AllMedia JOIN BelongsTo on AllMedia.mediaID = BelongsTo.mediaID
+        GROUP BY BelongsTo.genreName)
+SELECT PopularByGenre.genreName, AllMedia.mediaID, AllMedia.titleJPN, AllMedia.synopsis, AllMedia.rank, AllMedia.startDate, AllMedia.source
+FROM BelongsTo JOIN AllMedia ON BelongsTo.mediaID = AllMedia.mediaID JOIN PopularByGenre ON PopularByGenre.genreName = BelongsTo.genreName AND PopularByGenre.rank = AllMedia.rank WHERE PopularByGenre.genreName = genre;
+    END IF;
+END; //
+
+DELIMITER ;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS PopularSource //
+
+CREATE PROCEDURE PopularSource()
+BEGIN
+SELECT AllMedia.source AS sourceType, COUNT(AllMedia.mediaID) AS numShows
+FROM AllMedia
+WHERE AllMedia.rank <= 1000 AND (AllMedia.source = "Manga" OR AllMedia.source = "Original")
+GROUP BY AllMedia.source;
+END; //
+
+DELIMITER ;
