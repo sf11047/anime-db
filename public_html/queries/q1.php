01d8209c1806de6e91c94d5778a721ea6281c521
Samantha Fu <zongming04@gmail.com>
2021-05-08 22:50:11 -0400

    formatting

--- public_html/queries/q1.php
+++ public_html/queries/q1.php
@@ -6,11 +6,12 @@ include '../php/open.php';
 
 //Begin Query Code
 
-    $query = "Test Question";
+    $query = "Most Popular Anime by Genre";
     echo "<h1>".$query."<h1>";
+    
 
     $genreSelection = $_POST['genre'];
-    echo "genre: ".$genreSelection." <br> </br>";
+    echo "<h1> Genre: ".$genreSelection."<h1>";
 
     $myQuery = "Call PopByGenre(?);";
     $stmt = $conn->prepare($myQuery); 
@@ -18,7 +19,11 @@ include '../php/open.php';
     $stmt->execute();
     $result = $stmt->get_result();
     while ($row = $result->fetch_assoc()) {
-        echo "genre: ".$row['genreName']." ".$row['titleJPN']."<br></br>";
+        echo "<h2>".$row['titleJPN']."</h2>";
+        echo "<h3> Rank: ".$row['rank']."</h3>";
+        echo "<h3> Start Date: ".$row['startDate']."</h3>";
+        echo "<h3> Source: ".$row['source']."</h3>";
+        echo "<p> Rank: ".$row['synopsis']."</p>";
     }
     
 //End Query Code
