29a492b7620239b06ee7461cd7a6bc987032d0fe
Anthony D <adas22@jhu.edu>
2021-05-09 21:07:16 -0400

    huh shows doesn't work

--- public_html/queries/q10.php
+++ public_html/queries/q10.php
@@ -23,14 +23,30 @@ include '../php/open.php';
     $stmt->execute();
     $result = $stmt->get_result();
     while ($row = $result->fetch_assoc()) {
-        //array_push($dataPoints, array( "label"=> $row["genreName"], "y"=> 0));
-        echo "<p>Label: ".$row["genreName"]." Data: ".$row["count"]."</p>";
+        array_push($dataPoints, array( "label"=> $row["genreName"], "y"=> $row["count"]));
+       // echo "<p>Label: ".$row["genreName"]." Data: ".$row["count"]."</p>";
     }
     
 //End Query Code
 $conn->close();
 ?>
-
+<script>
+        window.onload = function () { 
+            var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: false,
+                exportEnabled: true,
+                theme: "light1", // "light1", "light2", "dark1", "dark2"
+                title:{
+                    text: "Number of Shows Most Highly Reviewed In Each Review Category"
+                },
+                data: [{
+                    type: "column", //change type to column, bar, line, area, pie, etc  
+                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                }]
+            });
+            chart.render(); 
+        }
+</script>
 <?php
 include "../php/query-template-end.php"; //HTML Template
 ?>
